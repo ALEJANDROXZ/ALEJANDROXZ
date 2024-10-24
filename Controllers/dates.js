def3eb01c706f9bf20b4a2bee4cb7c64464c3058
ALEJANDROXZ <69703932+ALEJANDROXZ@users.noreply.github.com>
2024-10-24 07:19:33 -0500

    Update dates.js

--- Controllers/dates.js
+++ Controllers/dates.js
@@ -1,27 +1,14 @@
-// Función para verificar si la sección está en vista
-function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-// Animar secciones al hacer scroll y cuando se carga la página
+// Mostrar las secciones y aplicar la animación al cargar y hacer scroll
 const sections = document.querySelectorAll("section");
 
-function animateSections() {
+function showSections() {
     sections.forEach(section => {
-        if (isInViewport(section)) {
-            section.classList.add("in-view");
-        }
+        section.classList.add("in-view");
     });
 }
 
-// Verificar la visibilidad de las secciones al cargar la página
-window.addEventListener("DOMContentLoaded", animateSections);
+// Mostrar las secciones al cargar la página
+window.addEventListener("DOMContentLoaded", showSections);
 
-// Verificar la visibilidad de las secciones al hacer scroll
-window.addEventListener("scroll", animateSections);
+// Mostrar las secciones al hacer scroll
+window.addEventListener("scroll", showSections);
